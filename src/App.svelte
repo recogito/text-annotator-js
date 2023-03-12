1d7fbbfd8f513e875d4f3f175ccddf3188fa6a71
Rainer Simon <rainer@rainersimon.io>
2023-03-12 17:26:02 +0100

    Project skeleton

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+
+</script>
+
+<div>Hello World</div>
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
+
+export default defineConfig({
+  plugins: [
+    svelte({ preprocess: sveltePreprocess() })
+  ],
+  server: {
+    open: '/test/index.html'
+  }
+});
