aa22fe5c0859a982c8f90375bab316638277b6f7
Rainer Simon <rainer@rainersimon.io>
2023-04-17 11:11:10 +0200

    Build + publishing config

--- src/TextAnnotator.ts
+++ src/TextAnnotator.ts
@@ -1,18 +1,18 @@
 import { createAnonymousGuest, type AnnotationLayer, type User, createLifecyleObserver, type PresenceProvider } from '@annotorious/core';
 import type { TextAnnotation } from './model/TextAnnotation';
 import { SelectionHandler } from './SelectionHandler';
-import { createTextStore } from './state';
+import { createTextStore, type TextAnnotationStore } from './state';
 import { createHighlightLayer } from './highlight/highlightLayer';
 import type { TextAnnotatorOptions } from './TextAnnotatorOptions';
 
 import './TextAnnotator.css';
 import { createPainter } from './presence/PresencePainter';
 
-export type TextAnnotationLayer = AnnotationLayer<TextAnnotation> & ReturnType<typeof TextAnnotator>;
+export type TextAnnotationLayer = AnnotationLayer<TextAnnotation> & ReturnType<typeof TextAnnotator> ;
 
 export const TextAnnotator = (container: HTMLElement, options: TextAnnotatorOptions = {}) => {
 
-  const store = createTextStore(container);
+  const store: TextAnnotationStore = createTextStore(container);
 
   const lifecycle = createLifecyleObserver(store.selection, store);
 
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,28 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   plugins: [
-    svelte({ preprocess: sveltePreprocess() })
+    svelte({ preprocess: sveltePreprocess() }),
+    dts({ insertTypesEntry: true })
   ],
   server: {
     open: '/test/index.html'
+  },
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: './src/index.ts',
+      name: 'RecogitoJS',
+      formats: ['es', 'umd'],
+      fileName: 'recogito-js'
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: 'recogito-js.[ext]'
+      }
+    }
   }
 });
