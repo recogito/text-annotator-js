29112fce4a2085c566776e99ccc79e6e5814cf2b
Rainer Simon <rainer@rainersimon.io>
2023-05-09 15:55:23 +0200

    Build tweaks

--- packages/recogito-react/src/TextAnnotator.tsx
+++ packages/recogito-react/src/TextAnnotator.tsx
@@ -1,25 +1,22 @@
-import { ReactNode, useContext, useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import { AnnotoriousContext } from '@annotorious/react';
 import { TextAnnotatorOptions, RecogitoJS } from '@recogito/recogito-js';
 
 export type TextAnnotatorProps = TextAnnotatorOptions & {
 
-  element?: string,
-
-  children?: ReactNode
+  element: string,
 
 }
 
 export const TextAnnotator = (props: TextAnnotatorProps) => {
 
-  const { element, children, ...opts } = props;
+  const { element, ...opts } = props;
 
   const { setAnno } = useContext(AnnotoriousContext);
 
   useEffect(() => {
     const el = element ? document.getElementById(element) : null;
     const r = RecogitoJS(el, opts);
-    console.log('initializing');
     setAnno(r);
   }, []);
 
