21caeb3692738de5ccb84afb4f83f9264695920b
Rainer Simon <rainer@rainersimon.io>
2023-06-29 10:26:39 +0200

    Convenience re-exports

--- packages/text-annotator-react/src/index.ts
+++ packages/text-annotator-react/src/index.ts
@@ -1,2 +1,8 @@
 export * from './TextAnnotator';
 export * from './TextAnnotatorPopup';
+
+// Re-export essential Types for convenience
+export type { 
+  TextAnnotation,
+  TextAnnotator as TextAnnotatorRef
+} from '@recogito/text-annotator';
