1b575ad0f4c43daee9a8a68d9d04066e3a80aabc
Rainer Simon <rainer@rainersimon.io>
2023-09-21 11:13:36 +0200

    Re-introduced inital DOM range revival

--- packages/plugin-tei/src/TEIPlugin.ts
+++ packages/plugin-tei/src/TEIPlugin.ts
@@ -6,15 +6,10 @@ import type {
 } from '@recogito/text-annotator';
 import type { 
   Annotator,
-  LifecycleEvents,
   Origin,  
   Store,  
-  // StoreChangeEvent,
-  // StoreObserveOptions 
 } from '@annotorious/core';
 import { 
-  // teiToTextAnnotation, 
-  // teiToTextTarget, 
   textToTEIAnnotation, 
   textToTEITarget 
 } from './crosswalk';
@@ -22,8 +17,6 @@ import type {
   TEIAnnotation,
   TEIAnnotationTarget
 } from './TEIAnnotation';
-
-// type TEIEvents = LifecycleEvents<TEIAnnotation>;
 
 export type TEIAnnotationStore = Store<TEIAnnotation> & {
 
@@ -44,53 +37,6 @@ export const TEIPlugin = (anno: RecogitoTextAnnotator): RecogitoTEIAnnotator => 
   const toTEI = textToTEIAnnotation(container);
 
   const toTEITarget = textToTEITarget(container);
-
-  /*
-  const observers: Array<{
-    onChange: (event: StoreChangeEvent<TEIAnnotation>) => void,
-    wrapped: (event: StoreChangeEvent<TextAnnotation>) => void
-  }> = [];
-
-  const listeners: Array<{
-    callback: TEIEvents[keyof TEIEvents],
-    wrapped: LifecycleEvents<TextAnnotation>[keyof TEIEvents]
-  }> = [];
-
-  // Wrap lifecycle handlers
-  const on = <E extends keyof TEIEvents>(event: E, callback: TEIEvents[E]) => {
-    let wrapped: LifecycleEvents<TextAnnotation>[E]; 
-
-    if (event === 'createAnnotation' || event === 'deleteAnnotation') {
-      // @ts-ignore
-      wrapped = (a: TextAnnotation) => callback(toTEI(a), undefined);
-      anno.on(event, wrapped);
-    } else if (event === 'selectionChanged') {
-      // @ts-ignore
-      wrapped = (a: TextAnnotation[]) => callback(a.map(toTEI));
-      anno.on(event, wrapped);
-    } else if (event === 'updateAnnotation') {
-      // @ts-ignore
-      wrapped =  (a: TextAnnotation, b: TextAnnotation) => callback(toTEI(a), teiTEI(b));
-      anno.on(event, wrapped);
-    } else if (event === 'viewportIntersect') {
-      // @ts-ignore
-      wrapped = (a: TextAnnotation[]) => callback(a.map(teiTEI));
-      anno.on(event, wrapped);
-    }
-
-    if (wrapped)
-      listeners.push({ callback, wrapped });
-  }
-
-  const off = <E extends keyof TEIEvents>(event: E, callback: TEIEvents[E]) => {
-    const idx = listeners.findIndex(listener => listener.callback === callback);
-    if (idx > -1) {
-      // @ts-ignore
-      anno.off(event, listeners[idx].wrapped);
-      listeners.splice(idx, 1);
-    }
-  }
-  */
 
   // Monkey-patch the store
   const store = anno.state.store as TextAnnotationStore;
@@ -120,56 +66,8 @@ export const TEIPlugin = (anno: RecogitoTextAnnotator): RecogitoTEIAnnotator => 
   store.updateTarget = (target: TEIAnnotationTarget | TextAnnotationTarget, origin: Origin) => 
     _updateTarget(toTEITarget(target), origin);
 
-  /*
-  const _observe = store.observe;
-  store.observe = (
-    onChange: { (event: StoreChangeEvent<TEIAnnotation>): void }, 
-    options: StoreObserveOptions = {}
-  ) => {
-    const wrapped = (event: StoreChangeEvent<TextAnnotation>) => {
-      const { changes, origin, state } = event;
-
-      const crosswalkedChanges = {
-        created: (changes.created || []).map(toTEI),
-        deleted: (changes.deleted || []).map(toTEI),
-        updated: (changes.updated || []).map(update => ({
-          ...update,
-          oldValue: toTEI(update.oldValue),
-          newValue: toTEI(update.newValue),
-          targetUpdated: update.targetUpdated ? ({ 
-            oldTarget: textToTEITarget(container)(update.targetUpdated.oldTarget as TextAnnotationTarget), 
-            newTarget: textToTEITarget(container)(update.targetUpdated.newTarget as TextAnnotationTarget)
-          }) : undefined
-        }))
-      }
-
-      onChange({ 
-        changes: crosswalkedChanges, 
-        origin, 
-        // TODO keep local copies. Otherwise every store change
-        // would mean we'll re-compute all DOM ranges!
-        state: state as unknown as TEIAnnotation[] 
-      });
-    };
-
-    _observe(wrapped, options);
-
-    return observers.push({ onChange, wrapped });
-  }
-
-  const _unobserve = store.unobserve;
-  store.unobserve = (onChange: { (event: StoreChangeEvent<TEIAnnotation>): void }) => {
-    const idx = observers.findIndex(observer => observer.onChange == onChange);
-    if (idx > -1) {
-      _unobserve(observers[idx].wrapped);
-      observers.splice(idx, 1);
-    }
-  }*/
-
   return {
     ...anno,
-    // on,
-    // off,
     state: {
       ...anno.state,
       // @ts-ignore
--- packages/text-annotator/src/state/TextAnnotatorState.ts
+++ packages/text-annotator/src/state/TextAnnotatorState.ts
@@ -12,6 +12,7 @@ import {
 import { IndexedHighlightRect, createSpatialTree } from './spatialTree';
 import type { TextAnnotation, TextAnnotationTarget } from '../model';
 import type { AnnotationRects, TextAnnotationStore } from './TextAnnotationStore';
+import { reviveTarget } from './reviveTarget';
 
 export type TextAnnotatorState = AnnotatorState<TextAnnotation> & {
 
@@ -39,6 +40,44 @@ export const createTextAnnotatorState = (
   const hover = createHoverState(store);
 
   const viewport = createViewportState();
+
+  // Wrap store interface to intercept annotations and revive DOM ranges, if needed
+  const addAnnotation = (annotation: TextAnnotation, origin = Origin.LOCAL) => {
+    const revived = annotation.target.selector.range instanceof Range ?
+      annotation : { ...annotation, target: reviveTarget(annotation.target, container) };
+
+    store.addAnnotation(revived, origin);
+  }
+
+  const bulkAddAnnotation = (annotations: TextAnnotation[], replace = true, origin = Origin.LOCAL) => {
+    const revived = annotations.map(a => a.target.selector.range instanceof Range ?
+      a : { ...a, target: reviveTarget(a.target, container )});
+
+    // Initial page load might take some time. Retry for more robustness.
+    const hasCollapsedRanges = revived.some(a => a.target.selector.range.collapsed);
+
+    if (hasCollapsedRanges) {
+      console.warn('Could not revive all targets');
+      console.warn(revived.filter(a => a.target.selector.range.collapsed));
+
+      const successful = revived.filter(a => !a.target.selector.range.collapsed);
+      store.bulkAddAnnotation(successful, replace, origin);
+    } else {
+      store.bulkAddAnnotation(revived, replace, origin);
+    }
+  }
+
+  const updateTarget = (target: TextAnnotationTarget, origin = Origin.LOCAL) => {
+    const revived = target.selector.range instanceof Range ?
+      target : reviveTarget(target, container);
+
+    store.updateTarget(revived, origin);
+  }
+
+  const bulkUpdateTargets = (targets: TextAnnotationTarget[], origin = Origin.LOCAL) => {
+    const revived = targets.map(t => t.selector.range instanceof Range ? t : reviveTarget(t, container));
+    store.bulkUpdateTargets(revived, origin);
+  }
 
   const getAt = (x: number, y: number): TextAnnotation | undefined => {
     const annotationId = tree.getAt(x, y);
@@ -111,12 +150,15 @@ export const createTextAnnotatorState = (
   return {
     store: {
       ...store,
+      addAnnotation,
+      bulkAddAnnotation,
+      bulkUpdateTargets,
       getAnnotationBounds,
       getAt,
       getIntersecting,
       getIntersectingRects,
       recalculatePositions,
-
+      updateTarget
     } as TextAnnotationStore,
     selection,
     hover,
