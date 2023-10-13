94e3d017a55ef390d7b17c9060148004d7b8900e
Rainer Simon <rainer@rainersimon.io>
2023-10-13 10:40:02 +0200

    Essential re-exports

--- packages/text-annotator/src/index.ts
+++ packages/text-annotator/src/index.ts
@@ -4,3 +4,12 @@ export * from './state';
 export * from './presence/PresencePainterOptions';
 export * from './TextAnnotator';
 export * from './TextAnnotatorOptions';
+
+// Re-export essentials from @annotorious/core utilities for convenience
+export * from '@annotorious/core/src/model';
+
+import {   
+  Origin as _Origin,
+} from '@annotorious/core/src/state';
+
+export const Origin = _Origin;
