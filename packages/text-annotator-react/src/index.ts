0194bd968a2fa6e1efbb1f2d0eca11a757d46add
Rainer Simon <rainer@rainersimon.io>
2023-10-13 13:32:31 +0200

    Export fix

--- packages/text-annotator-react/src/index.ts
+++ packages/text-annotator-react/src/index.ts
@@ -5,5 +5,5 @@ export * from './TextAnnotatorPopup';
 // Re-export essential Types for convenience
 export type { 
   TextAnnotation,
-  TextAnnotator
+  TextAnnotator as RecogitoTextAnnotator
 } from '@recogito/text-annotator';
