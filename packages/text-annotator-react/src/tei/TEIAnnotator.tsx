c98b1ab14f420b26b35d281bcfca32d0a9c08bae
Rainer Simon <rainer@rainersimon.io>
2023-10-17 11:20:25 +0200

    TS tweaks

--- packages/text-annotator-react/src/tei/TEIAnnotator.tsx
+++ packages/text-annotator-react/src/tei/TEIAnnotator.tsx
@@ -7,7 +7,7 @@ import '@recogito/text-annotator/dist/text-annotator.css';
 
 export type TEIAnnotatorProps = TextAnnotatorOptions & {
 
-  children?: ReactNode;
+  children?: ReactNode | JSX.Element;
 
   formatter?: Formatter;
 
