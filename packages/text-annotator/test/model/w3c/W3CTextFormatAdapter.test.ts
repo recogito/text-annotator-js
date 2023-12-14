2018b1eb9b23282fc7819e6374ae50e0b16a5506
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2023-12-14 11:03:16 +0200

    Added comparison against the fixture

--- packages/text-annotator/test/model/w3c/W3CTextFormatAdapter.test.ts
+++ packages/text-annotator/test/model/w3c/W3CTextFormatAdapter.test.ts
@@ -1,7 +1,8 @@
 import { describe, it, beforeEach, afterEach, expect } from 'vitest';
 import { JSDOM } from 'jsdom';
 import { correctTextAnnotation } from './fixtures';
-import { parseW3CTextAnnotation } from '../../../src/model/w3c';
+import { parseW3CTextAnnotation, type W3CTextAnnotationTarget } from '../../../src/model/w3c';
+import { W3CAnnotationBody } from '@annotorious/core/src/model/W3CAnnotation';
 
 beforeEach(async () => {
   const dom = await JSDOM.fromFile(`${process.cwd()}/test/index.html`);
@@ -19,13 +20,16 @@ describe('parseW3CTextAnnotation', () => {
     const parsed = parseW3CTextAnnotation(correctTextAnnotation, global.contentContainer);
     expect(parsed.error).toBeUndefined();
 
+    const fixtureBody = correctTextAnnotation.body as W3CAnnotationBody;
+    const fixtureTarget = correctTextAnnotation.target as W3CTextAnnotationTarget;
+
     const { parsed: textAnnotation } = parsed;
     expect(textAnnotation.bodies).toHaveLength(1);
+    expect(textAnnotation.bodies[0].value).toBe(fixtureBody.value);
 
     const { quote, start, end, range } = textAnnotation.target.selector;
-    expect(quote).toStrictEqual('But as years');
-    expect(start).toBeDefined();
-    expect(end).toBeDefined();
+    expect(quote).toStrictEqual(fixtureTarget.selector[0].exact);
+    expect(start).toStrictEqual(fixtureTarget.selector[1].start);
     expect(range).toBeDefined();
   });
 });
