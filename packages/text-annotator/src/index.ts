f6f5f02497f6f112586ca036259f6f4912ffe12e
Rainer Simon <rainer@rainersimon.io>
2023-12-21 16:02:10 +0100

    Minor re-export fix

--- packages/text-annotator/src/index.ts
+++ packages/text-annotator/src/index.ts
@@ -4,3 +4,9 @@ export * from './state';
 export * from './presence/PresencePainterOptions';
 export * from './TextAnnotator';
 export * from './TextAnnotatorOptions';
+
+import {
+  Origin as _Origin,
+} from '@annotorious/core';
+
+export { _Origin as Origin };
