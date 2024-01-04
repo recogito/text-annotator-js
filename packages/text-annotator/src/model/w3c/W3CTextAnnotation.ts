b758e4ef0093d8454c12b9e0ae9540f1c5944f0d
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2024-01-04 16:02:19 +0200

    Added `W3CTextAnnotation` type definition

---
+++ packages/text-annotator/src/model/w3c/W3CTextAnnotation.ts
@@ -0,0 +1,45 @@
+import type { W3CAnnotation, W3CAnnotationTarget } from '@annotorious/core';
+
+export interface W3CTextAnnotation extends W3CAnnotation {
+
+  target: W3CTextAnnotationTarget | W3CTextAnnotationTarget[];
+
+}
+
+export interface W3CTextAnnotationTarget extends W3CAnnotationTarget {
+
+  selector: W3CTextSelector | W3CTextSelector[];
+
+}
+
+/**
+ * Matches the `Text Quote Selector` spec
+ * @see https://www.w3.org/TR/annotation-model/#text-quote-selector
+ */
+export interface W3CTextQuoteSelector {
+
+  type: 'TextQuoteSelector';
+
+  exact: string;
+
+  prefix?: string;
+
+  suffix?: string;
+
+}
+
+/**
+ * Matches the `Text Position Selector` spec
+ * @see https://www.w3.org/TR/annotation-model/#text-position-selector
+ */
+export interface W3CTextPositionSelector {
+
+  type: 'TextPositionSelector';
+
+  start: number;
+
+  end: number;
+
+}
+
+export type W3CTextSelector = W3CTextQuoteSelector | W3CTextPositionSelector;
