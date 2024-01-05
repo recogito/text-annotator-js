787ca8e3188ef8cf49d7df0c57ef3702f28f951b
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2024-01-05 14:56:43 +0200

    Added test definition for the `W3CTextFormatAdapter`

---
+++ packages/text-annotator/test/model/w3c/fixtures.ts
@@ -0,0 +1,42 @@
+import type { W3CTextAnnotation } from '../../../src';
+
+export const textAnnotation: W3CTextAnnotation = {
+  '@context': 'http://www.w3.org/ns/anno.jsonld',
+  type: 'Annotation',
+  id: 'http://www.example.com/annotation/ccf99248-e2a0-4fea-9869-46da99390a9b',
+  created: '2023-01-28T12:00:00Z',
+  creator: {
+    type: 'Person',
+    id: 'qn7zdU8XD6ZPDBMXdc7w',
+    name: 'Oleksandr'
+  },
+  body: [{
+    type: 'TextualBody',
+    value: 'A comment about a content'
+  }],
+  target: {
+    source: 'http://www.example.com/source/1',
+    selector: [
+      {
+        type: 'TextQuoteSelector',
+        exact: 'But as years',
+        prefix: 'arry him. ',
+        suffix: ' went by, '
+      },
+      {
+        type: 'TextPositionSelector',
+        start: 922,
+        end: 934
+      },
+    ]
+  }
+};
+
+export const incompleteTextAnnotation: W3CTextAnnotation = {
+  ...textAnnotation,
+  target: {
+    ...textAnnotation.target,
+    // @ts-ignore
+    selector: textAnnotation.target.selector[0] // Only the `Text Quote Selector`
+  }
+};
