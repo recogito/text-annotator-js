32cd6a9fb78ad7afc439cb2d72bb5e44632c06e5
Rainer Simon <rainer@rainersimon.io>
2024-01-15 08:31:12 +0100

    Minor bugfixes + upgraded to latest dependecy versions

--- packages/text-annotator-react/src/tei/CETEIcean.tsx
+++ packages/text-annotator-react/src/tei/CETEIcean.tsx
@@ -7,6 +7,46 @@ interface CETEIceanProps {
 
   onLoad?(element: Element): void;
 
+  behaviors?: any;
+
+}
+
+// Override default list, note, table, and ref behaviors 
+// so they don't introduce text into the DOM that interferes
+// with annotation. Apply a patch to graphics for robustness.
+const PRESET_BEHAVIORS = {
+  tei: {
+    ref: (elem: Element) => {
+      const a = document.createElement('a');
+
+      while(elem.firstChild) {
+        a.appendChild(elem.removeChild(elem.firstChild));
+      }
+
+      a.setAttribute('href', elem.getAttribute('target'));
+
+      elem.appendChild(a);
+    },
+    // Cf. https://github.com/TEIC/CETEIcean/issues/67
+    graphic: (elem: Element) => {
+      const content = new Image();
+      content.src = elem.getAttribute('url')?.trim();
+
+      if (elem.hasAttribute('width'))
+        content.setAttribute('width', elem.getAttribute('width'));
+
+      if (elem.hasAttribute('height'))
+        content.setAttribute('height', elem.getAttribute('height'));
+
+      elem.appendChild(content);
+    },
+    list: null,
+    note: null,
+    table: null,
+    teiHeader: (elem: HTMLElement) => {
+      elem.hidden = true;
+    }
+  }
 }
 
 export const CETEIcean = (props: CETEIceanProps) => {
@@ -17,28 +57,12 @@ export const CETEIcean = (props: CETEIceanProps) => {
     if (props.tei) {
       const ceteicean = new CETEI();
 
-      // Override default list, note, table, and ref behaviors 
-      // so they don't introduce text into the DOM that interferes
-      // with annotation
       ceteicean.addBehaviors({
+        ...PRESET_BEHAVIORS,
+        ...(props.behaviors || {}),
         tei: {
-          ref: (elem: Element) => {
-            const a = document.createElement('a');
-
-            while(elem.firstChild) {
-              a.appendChild(elem.removeChild(elem.firstChild));
-            }
-
-            a.setAttribute('href', elem.getAttribute('target'));
-
-            elem.appendChild(a);
-          },
-          list: null,
-          note: null,
-          table: null,
-          teiHeader: (elem: HTMLElement) => {
-            elem.hidden = true;
-          }
+          ...PRESET_BEHAVIORS.tei,
+          ...(props.behaviors?.tei || {})
         }
       });
 
