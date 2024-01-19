4ab87c2fb49adc9487e7d2e27d4e75e093760a80
Rainer Simon <rainer@rainersimon.io>
2024-01-19 19:27:06 +0100

    Style tweaks

--- packages/text-annotator/src/model/w3c/W3CTextFormatAdapter.ts
+++ packages/text-annotator/src/model/w3c/W3CTextFormatAdapter.ts
@@ -8,7 +8,7 @@ import {
 } from '@annotorious/core';
 import type { TextAnnotation, TextSelector } from '../core';
 import type { W3CTextAnnotation, W3CTextSelector } from '../w3c';
-import { reviveTargetRange } from '../../state';
+import { reviveTarget } from '../../state';
 import { getQuoteContext } from '../../utils';
 
 export type W3CTextFormatAdapter = FormatAdapter<TextAnnotation, W3CTextAnnotation>;
@@ -18,12 +18,18 @@ export type W3CTextFormatAdapter = FormatAdapter<TextAnnotation, W3CTextAnnotati
  * @param container - the HTML container of the annotated content,
  *                    Required to locate the content's `range` within the DOM
  */
-export const W3CTextFormat = (source: string, container: HTMLElement): W3CTextFormatAdapter => ({
+export const W3CTextFormat = (
+  source: string, 
+  container: HTMLElement
+): W3CTextFormatAdapter => ({
   parse: (serialized) => parseW3CTextAnnotation(serialized, container),
   serialize: (annotation) => serializeW3CTextAnnotation(annotation, source, container)
 });
 
-export const parseW3CTextAnnotation = (annotation: W3CTextAnnotation, container: HTMLElement): ParseResult<TextAnnotation> => {
+export const parseW3CTextAnnotation = (
+  annotation: W3CTextAnnotation, 
+  container: HTMLElement
+): ParseResult<TextAnnotation> => {
   const annotationId = annotation.id || uuidv4();
 
   const {
@@ -36,8 +42,11 @@ export const parseW3CTextAnnotation = (annotation: W3CTextAnnotation, container:
 
   const bodies = parseW3CBodies(body, annotationId);
 
-  const w3cTarget = Array.isArray(annotation.target) ? annotation.target[0] : annotation.target;
-  const w3cSelectors = Array.isArray(w3cTarget.selector) ? w3cTarget.selector : [w3cTarget.selector];
+  const w3cTarget = Array.isArray(annotation.target) 
+    ? annotation.target[0] : annotation.target;
+
+  const w3cSelectors = Array.isArray(w3cTarget.selector) 
+    ? w3cTarget.selector : [ w3cTarget.selector ];
 
   const selector = w3cSelectors.reduce<Partial<TextSelector>>((s, w3cSelector) => {
     switch (w3cSelector.type) {
@@ -52,41 +61,44 @@ export const parseW3CTextAnnotation = (annotation: W3CTextAnnotation, container:
     return s;
   }, {});
 
-  if (selector.start !== undefined && selector.end !== undefined) {
-    selector.range = reviveTargetRange(selector.start, selector.end, container);
-  }
+  const isTextSelector= (selector: Partial<TextSelector>): selector is TextSelector =>
+    selector.quote !== undefined && selector.start !== undefined && selector.end !== undefined;
 
-  return isCompleteTextSelector(selector) ? {
-    parsed: {
-      ...rest,
-      id: annotationId,
-      bodies,
-      target: {
-        created: created ? new Date(created) : undefined,
-        creator: parseW3CUser(creator),
-        updated: modified ? new Date(modified) : undefined,
-        ...rest.target,
-        annotation: annotationId,
-        selector
+  if (isTextSelector(selector)) {
+    return {
+      parsed: {
+        ...rest,
+        id: annotationId,
+        bodies,
+        target: reviveTarget({
+          ...rest.target,
+          created: created ? new Date(created) : undefined,
+          creator: parseW3CUser(creator),
+          updated: modified ? new Date(modified) : undefined,
+          annotation: annotationId,
+          selector
+        }, container)
       }
     }
-  } : { error: getParsingError(selector) };
-
-  function isCompleteTextSelector(selector: Partial<TextSelector>): selector is TextSelector {
-    return selector.quote !== undefined && selector.range !== undefined;
-  }
-
-  function getParsingError(incompleteSelector: Partial<TextSelector>): Error {
-    let missingTypes = [
-      !incompleteSelector.start ? 'TextPositionSelector' : undefined,
-      !incompleteSelector.quote ? 'TextQuoteSelector' : undefined
+  } else {
+    const missingTypes = [
+      !selector.start ? 'TextPositionSelector' : undefined,
+      !selector.quote ? 'TextQuoteSelector' : undefined
     ].filter(Boolean);
-    return Error(`Missing selector types: ${missingTypes.join(' and ')}`);
-  }
-};
 
-export const serializeW3CTextAnnotation = (annotation: TextAnnotation, source: string, container: HTMLElement): W3CTextAnnotation => {
+    return {
+      error: Error(`Missing selector types: ${missingTypes.join(' and ')}`)
+    }
+  }
+}
+
+export const serializeW3CTextAnnotation = (
+  annotation: TextAnnotation, 
+  source: string, 
+  container: HTMLElement
+): W3CTextAnnotation => {
   const { bodies, target, ...rest } = annotation;
+
   const {
     selector,
     creator,
@@ -94,22 +106,21 @@ export const serializeW3CTextAnnotation = (annotation: TextAnnotation, source: s
     updated,
     ...targetRest
   } = target;
+
   const { quote, start, end, range } = selector;
+
   const { prefix, suffix } = getQuoteContext(range, container);
 
-  const w3cSelector: W3CTextSelector[] = [
-    {
-      type: 'TextQuoteSelector',
-      exact: quote,
-      prefix,
-      suffix
-    },
-    {
-      type: 'TextPositionSelector',
-      start,
-      end
-    }
-  ];
+  const w3cSelector: W3CTextSelector[] = [{
+    type: 'TextQuoteSelector',
+    exact: quote,
+    prefix,
+    suffix
+  },{
+    type: 'TextPositionSelector',
+    start,
+    end
+  }];
 
   return {
     ...rest,
@@ -125,5 +136,6 @@ export const serializeW3CTextAnnotation = (annotation: TextAnnotation, source: s
       source,
       selector: w3cSelector
     }
-  };
-};
+  }
+
+}
