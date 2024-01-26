dc8c6b888e7234cb23aeda71dae03b6f06ea9be4
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2024-01-26 13:19:22 +0200

    Added React strict mode for the test app

--- packages/text-annotator-react/test/index.tsx
+++ packages/text-annotator-react/test/index.tsx
@@ -1,7 +1,10 @@
-import React, { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 import { App } from './App';
 
-const root = createRoot(document.getElementById('root') as Element);
-root.render(
-  <App />
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
