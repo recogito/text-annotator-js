887ceb51bcabee85bdea5cff4a7ccb795d1c1317
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2024-02-09 10:58:24 +0200

    Added `rangeToSelector` utility

--- packages/text-annotator/src/utils/index.ts
+++ packages/text-annotator/src/utils/index.ts
@@ -2,4 +2,6 @@ export * from './getClientRectsPonyfill';
 export * from './getQuoteContext';
 export * from './mergeClientRects';
 export * from './trimRange';
+export * from './getAnnotableRanges';
+export * from './rangeToSelector';
 export * from './debounce';
