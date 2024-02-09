6bace3f4fb6ed721e6434f540b179162fdd403e2
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2024-02-09 16:29:58 +0200

    Added revive target range utility that skips not annotable elements

--- packages/text-annotator/src/state/reviveTarget.ts
+++ packages/text-annotator/src/state/reviveTarget.ts
@@ -1,4 +1,67 @@
 import type { TextAnnotationTarget } from '../model';
+import { notAnnotableSelector } from '../utils';
+
+/**
+ * Retrieves the DOM range from the given text annotation position
+ * Only the annotable elements are processed and counted towards the range
+ *
+ * @param start start of the annotated content
+ * @param end end of the annotated content
+ * @param offsetReference the HTML container of the annotated content
+ *
+ * @returns the DOM range
+ */
+export const reviveTargetRange = (start: number, end: number, offsetReference: HTMLElement): Range => {
+  const iterator = document.createNodeIterator(offsetReference, NodeFilter.SHOW_TEXT, (node) =>
+    node.parentElement?.closest(notAnnotableSelector)
+      ? NodeFilter.FILTER_SKIP
+      : NodeFilter.FILTER_ACCEPT
+  );
+
+  // Position that contains the length of the preceding annotable text nodes
+  let runningOffset = 0;
+
+  const range = document.createRange();
+
+  let n = iterator.nextNode();
+  if (n === null) console.error('Could not revive annotation target. Content missing.');
+
+  // If there's no offset reference, start immediately
+  let startCounting = !offsetReference;
+  while (n !== null) {
+    startCounting ||= offsetReference?.contains(n);
+
+    if (startCounting) {
+      const len = n.textContent?.length || 0;
+
+      if (runningOffset + len > start) {
+        range.setStart(n, start - runningOffset);
+        break;
+      }
+
+      runningOffset += len;
+    }
+
+    n = iterator.nextNode();
+  }
+
+  // set range end
+  while (n !== null) {
+    const len = n.textContent?.length || 0;
+
+    if (runningOffset + len >= end) {
+      range.setEnd(n, end - runningOffset);
+      break;
+    }
+
+    runningOffset += len;
+
+    n = iterator.nextNode();
+  }
+
+  return range;
+};
+
 
 /**
  * Constructs a new target with the recalculated DOM range from the given text annotation target
@@ -15,58 +78,12 @@ export const reviveTarget = (
   const { start, end } = target.selector;
 
   const offsetReference = target.selector.offsetReference ? target.selector.offsetReference : container;
-  if (!offsetReference)
-    return target;
-
-  const iterator = document.createNodeIterator(container, NodeFilter.SHOW_TEXT);
-
-  let runningOffset = 0;
-
-  let range = document.createRange();
-
-  let n = iterator.nextNode();
-  if (n === null)
-    console.error('Could not revive annotation target. Content missing.');
-
-  // If there's no offset reference, start immediately
-  let startCounting = !offsetReference;
-  while (n !== null) {
-    const len = n.textContent.length;
-
-    if (!startCounting && offsetReference)
-      startCounting = offsetReference.contains(n);
-
-    if (startCounting) {
-      if (runningOffset + len > start) {
-        range.setStart(n, start - runningOffset);
-        break;
-      }
-  
-      runningOffset += len;  
-    }
-
-    n = iterator.nextNode();
-  }
-
-  // set range end
-  while (n !== null) {
-    const len = n.textContent.length;
-
-    if (runningOffset + len >= end) {
-      range.setEnd(n, end - runningOffset);
-      break;
-    }
-
-    runningOffset += len;
-
-    n = iterator.nextNode();
-  }
-
-  return {
+  return offsetReference ? {
     ...target,
-    selector: { 
+    selector: {
       ...target.selector,
-      range
+      range: reviveTargetRange(start, end, offsetReference)
     }
-  }
+  } : target;
+
 }
