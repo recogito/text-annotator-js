49b69447b45730a693614d153dd1802e1ab9c4ff
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2024-02-12 14:52:01 +0200

    Added w3c parse method for the multiple selectors

--- packages/text-annotator/src/model/w3c/W3CTextFormatAdapter.ts
+++ packages/text-annotator/src/model/w3c/W3CTextFormatAdapter.ts
@@ -6,10 +6,9 @@ import {
   parseW3CUser,
   serializeW3CBodies
 } from '@annotorious/core';
-import type { TextAnnotation, TextSelector } from '../core';
+import type { TextAnnotation, TextAnnotationTarget, TextSelector } from '../core';
 import type { W3CTextAnnotation, W3CTextSelector } from '../w3c';
-import { reviveTarget } from '../../state';
-import { getQuoteContext } from '../../utils';
+import { getQuoteContext, reviveSelector, reviveTarget } from '../../utils';
 
 export type W3CTextFormatAdapter = FormatAdapter<TextAnnotation, W3CTextAnnotation>;
 
@@ -19,15 +18,68 @@ export type W3CTextFormatAdapter = FormatAdapter<TextAnnotation, W3CTextAnnotati
  *                    Required to locate the content's `range` within the DOM
  */
 export const W3CTextFormat = (
-  source: string, 
+  source: string,
   container: HTMLElement
 ): W3CTextFormatAdapter => ({
   parse: (serialized) => parseW3CTextAnnotation(serialized, container),
   serialize: (annotation) => serializeW3CTextAnnotation(annotation, source, container)
 });
 
+const isTextSelector = (selector: Partial<TextSelector>): selector is TextSelector =>
+  selector.quote !== undefined && selector.start !== undefined && selector.end !== undefined;
+
+const parseW3CTextTargets = (annotation: W3CTextAnnotation, container: HTMLElement) => {
+  const {
+    id: annotationId,
+    creator,
+    created,
+    modified,
+    target
+  } = annotation;
+
+  const w3cTargets = Array.isArray(target) ? target : [target];
+
+  const parsed: TextAnnotationTarget = {
+    creator: parseW3CUser(creator),
+    created: created ? new Date(created) : undefined,
+    updated: modified ? new Date(modified) : undefined,
+    annotation: annotationId,
+    selector: []
+  };
+
+  for (const w3cTarget of w3cTargets) {
+    const w3cSelectors = Array.isArray(w3cTarget.selector) ? w3cTarget.selector : [w3cTarget.selector];
+
+    const selector = w3cSelectors.reduce<Partial<TextSelector>>((s, w3cSelector) => {
+      switch (w3cSelector.type) {
+        case 'TextQuoteSelector':
+          s.quote = w3cSelector.exact;
+          break;
+        case 'TextPositionSelector':
+          s.start = w3cSelector.start;
+          s.end = w3cSelector.end;
+          break;
+      }
+      return s;
+    }, {});
+
+    if (isTextSelector(selector)) {
+      parsed.selector.push(reviveSelector(selector, container));
+    } else {
+      const missingTypes = [
+        !selector.start ? 'TextPositionSelector' : undefined,
+        !selector.quote ? 'TextQuoteSelector' : undefined
+      ].filter(Boolean);
+
+      return { error: Error(`Missing selector types: ${missingTypes.join(' and ')} for annotation: ${annotation.id}`) };
+    }
+  }
+
+  return { parsed };
+};
+
 export const parseW3CTextAnnotation = (
-  annotation: W3CTextAnnotation, 
+  annotation: W3CTextAnnotation,
   container: HTMLElement
 ): ParseResult<TextAnnotation> => {
   const annotationId = annotation.id || uuidv4();
@@ -41,60 +93,23 @@ export const parseW3CTextAnnotation = (
   } = annotation;
 
   const bodies = parseW3CBodies(body, annotationId);
+  const target = parseW3CTextTargets(annotation, container);
 
-  const w3cTarget = Array.isArray(annotation.target) 
-    ? annotation.target[0] : annotation.target;
-
-  const w3cSelectors = Array.isArray(w3cTarget.selector) 
-    ? w3cTarget.selector : [ w3cTarget.selector ];
-
-  const selector = w3cSelectors.reduce<Partial<TextSelector>>((s, w3cSelector) => {
-    switch (w3cSelector.type) {
-      case 'TextQuoteSelector':
-        s.quote = w3cSelector.exact;
-        break;
-      case 'TextPositionSelector':
-        s.start = w3cSelector.start;
-        s.end = w3cSelector.end;
-        break;
-    }
-    return s;
-  }, {});
-
-  const isTextSelector= (selector: Partial<TextSelector>): selector is TextSelector =>
-    selector.quote !== undefined && selector.start !== undefined && selector.end !== undefined;
-
-  if (isTextSelector(selector)) {
-    return {
+  return 'error' in target
+    ? { error: target.error }
+    : {
       parsed: {
         ...rest,
         id: annotationId,
         bodies,
-        target: reviveTarget({
-          ...rest.target,
-          created: created ? new Date(created) : undefined,
-          creator: parseW3CUser(creator),
-          updated: modified ? new Date(modified) : undefined,
-          annotation: annotationId,
-          selector
-        }, container)
+        target: target.parsed
       }
-    }
-  } else {
-    const missingTypes = [
-      !selector.start ? 'TextPositionSelector' : undefined,
-      !selector.quote ? 'TextQuoteSelector' : undefined
-    ].filter(Boolean);
-
-    return {
-      error: Error(`Missing selector types: ${missingTypes.join(' and ')}`)
-    }
-  }
-}
+    };
+};
 
 export const serializeW3CTextAnnotation = (
-  annotation: TextAnnotation, 
-  source: string, 
+  annotation: TextAnnotation,
+  source: string,
   container: HTMLElement
 ): W3CTextAnnotation => {
   const { bodies, target, ...rest } = annotation;
@@ -116,7 +131,7 @@ export const serializeW3CTextAnnotation = (
     exact: quote,
     prefix,
     suffix
-  },{
+  }, {
     type: 'TextPositionSelector',
     start,
     end
@@ -136,6 +151,6 @@ export const serializeW3CTextAnnotation = (
       source,
       selector: w3cSelector
     }
-  }
+  };
 
-}
+};
