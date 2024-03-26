fca4a185fdec4f1b8f7c6709789fef0f1c4ad6f3
Rainer Simon <rainer@rainersimon.io>
2024-03-26 13:26:26 +0100

    Cleanup

--- packages/text-annotator-react/src/index.ts
+++ packages/text-annotator-react/src/index.ts
@@ -1,9 +1,12 @@
 export * from './tei';
 export * from './TextAnnotator';
 export * from './TextAnnotatorPopup';
-export * from './TextAnnotatorPlugin';
 
 // Re-export essential Types for convenience
+export type {
+  AnnotoriousPlugin
+} from '@annotorious/react';
+
 export type { 
   TextAnnotation,
   TextAnnotator as RecogitoTextAnnotator
