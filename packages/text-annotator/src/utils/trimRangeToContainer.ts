e96937ad6e966e7fd8c0fca9d813c6ba6c78056c
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2024-10-10 15:59:07 +0300

    Added "select all" handling for the range-to-container trimming

--- packages/text-annotator/src/utils/trimRangeToContainer.ts
+++ packages/text-annotator/src/utils/trimRangeToContainer.ts
@@ -1,26 +1,32 @@
+import { rangeContains } from './rangeContains';
+
 export const trimRangeToContainer = (
   range: Range,
   container: HTMLElement
 ): Range => {
   const trimmedRange = range.cloneRange();
 
-  const containsStart = container.contains(trimmedRange.startContainer);
-  const containsEnd = container.contains(trimmedRange.endContainer);
+  const containsRangeStart = container.contains(trimmedRange.startContainer);
+  const containsRangeEnd = container.contains(trimmedRange.endContainer);
 
   /**
-   * If both the start and the end are outside the container -
-   * collapse such a range as irrelevant
+   * If both range's edges are not within the container (i.e. the selection is done outside)
+   * and the range doesn't cover the container itself (i.e. "Select All" was pressed) ->
+   * collapse it as irrelevant
    */
-  if (!containsStart && !containsEnd) {
-    trimmedRange.collapse();
-    return trimmedRange;
+  if (!containsRangeStart && !containsRangeEnd) {
+    const containedWithinRange = rangeContains(trimmedRange, container);
+    if (!containedWithinRange) {
+      trimmedRange.collapse();
+      return trimmedRange;
+    }
   }
 
   /**
    * If the range starts outside the container -
    * trim it to the start of the container
    */
-  if (!containsStart) {
+  if (!containsRangeStart) {
     trimmedRange.setStart(container, 0);
   }
 
@@ -28,7 +34,7 @@ export const trimRangeToContainer = (
    * If the range ends outside the container -
    * trim it to the end of the container
    */
-  if (!containsEnd) {
+  if (!containsRangeEnd) {
     trimmedRange.setEnd(container, container.childNodes.length);
   }
 
