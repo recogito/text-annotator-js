68d5e268dd03b0f82f4cc5edc932566317d0e1d6
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2024-10-14 18:58:13 +0300

    Removed unused `getAnnotatableFragment`

--- packages/text-annotator/src/utils/index.ts
+++ packages/text-annotator/src/utils/index.ts
@@ -2,7 +2,6 @@ export * from './cancelSingleClickEvents';
 export * from './device';
 export * from './programmaticallyFocusable';
 export * from './debounce';
-export * from './getAnnotatableFragment';
 export * from './getQuoteContext';
 export * from './isWhitespaceOrEmpty';
 export * from './isRevived';
