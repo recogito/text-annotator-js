ecd49de53988e1035119a260b7b838cd151cefd0
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2024-10-14 19:20:06 +0300

    Simplified the `isNotAnnotatable` arg type

--- packages/text-annotator/src/utils/splitAnnotatableRanges.ts
+++ packages/text-annotator/src/utils/splitAnnotatableRanges.ts
@@ -2,7 +2,7 @@ export const NOT_ANNOTATABLE_CLASS = 'not-annotatable';
 
 export const NOT_ANNOTATABLE_SELECTOR = `.${NOT_ANNOTATABLE_CLASS}`;
 
-export const isNotAnnotatable = <T extends Node>(node: T): boolean => {
+export const isNotAnnotatable = (node: Node): boolean => {
   const closestNotAnnotatable = node instanceof HTMLElement
     ? node.closest(NOT_ANNOTATABLE_SELECTOR)
     : node.parentElement?.closest(NOT_ANNOTATABLE_SELECTOR);
