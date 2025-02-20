c8bb03c91cced9f46ab4e714ff19401b00da1292
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2025-02-20 13:28:57 +0200

    Added `externalizeDeps` for the `@recogito/react-text-annotator`

--- packages/text-annotator-react/vite.config.ts
+++ packages/text-annotator-react/vite.config.ts
@@ -2,23 +2,22 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import dts from 'vite-plugin-dts';
+import { externalizeDeps } from 'vite-plugin-externalize-deps';
 
-import * as packageJson from './package.json';
-
-export default defineConfig(({ command, mode }) => ({
+export default defineConfig({
   plugins: [
     react(),
     tsConfigPaths(),
     dts({ 
       include: ['./src/'],
       entryRoot: './src'
-    })
+    }),
+    externalizeDeps(),
   ],
   server: {
     open: '/test/index.html'
   },
   build: {
-    minify: false,
     lib: {
       entry: './src/index.ts',
       name: 'ReactTextAnnotator',
@@ -26,18 +25,10 @@ export default defineConfig(({ command, mode }) => ({
       fileName: (format) => `react-text-annotator.${format}.js`
     },
     rollupOptions: {
-      external: [
-        ...Object.keys(packageJson.peerDependencies),
-        "@annotorious/core",
-        "@annotorious/react",
-        "@recogito/text-annotator",
-        "@recogito/text-annotator-tei"
-      ],
       output: {
-        preserveModules: true,
         assetFileNames: 'react-text-annotator.[ext]'
       }
     },
     sourcemap: true
   }
-}));
+});
--- packages/text-annotator/vite.config.ts
+++ packages/text-annotator/vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-
 import dts from 'vite-plugin-dts';
 import { externalizeDeps } from 'vite-plugin-externalize-deps';
 
