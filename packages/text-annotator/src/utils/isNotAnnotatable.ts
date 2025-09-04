0d5e28dd92fdf40f8117ba737c202fad17b7e9e5
Rainer Simon <rainer@rainersimon.io>
2025-09-04 09:42:28 +0200

    Various tweaks

--- packages/text-annotator/src/utils/isNotAnnotatable.ts
+++ packages/text-annotator/src/utils/isNotAnnotatable.ts
@@ -2,14 +2,16 @@ export const NOT_ANNOTATABLE_CLASS = 'not-annotatable';
 
 export const NOT_ANNOTATABLE_SELECTOR = `.${NOT_ANNOTATABLE_CLASS}`;
 
-export const isNotAnnotatable = (node: Node): boolean => {
+export const isNotAnnotatable = (container: Node, node: Node): boolean => {
+  if (!container.contains(node)) return true;
+
   const closestNotAnnotatable = node instanceof HTMLElement
     ? node.closest(NOT_ANNOTATABLE_SELECTOR)
     : node.parentElement?.closest(NOT_ANNOTATABLE_SELECTOR);
   return Boolean(closestNotAnnotatable);
 }
 
-export const isRangeAnnotatable = (range: Range): boolean => {
+export const isRangeAnnotatable = (container: Node, range: Range): boolean => {
   const ancestor = range.commonAncestorContainer;
-  return !isNotAnnotatable(ancestor);
+  return !isNotAnnotatable(container, ancestor);
 }
