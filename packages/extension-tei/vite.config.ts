a4d939778031e04e55c4c40b0b4e8ccf3cd9c7a1
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2025-09-08 14:50:28 +0300

    Added declarations generation with `tsc`

--- packages/extension-tei/vite.config.ts
+++ packages/extension-tei/vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [
-    dts({ 
-      insertTypesEntry: true,
-      entryRoot: '.'
-    })
-  ],
   server: {
     open: '/test/index.html'
   },
   build: {
+    emptyOutDir: false,
     sourcemap: true,
     lib: {
       entry: './src/index.ts',
